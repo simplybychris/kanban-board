bbc0b5cdfe75ee7374ea43fc8f5570eb70aaf7b1
simplybychris <59068947+simplybychris@users.noreply.github.com>
2021-08-29 12:00:48 +0200

    Tasks component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Task } from './task/interfaces/task';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kanban-board';
+  todo: Task[] = [
+    {
+      title: 'Learn new framework',
+      description: "Find interesting framework and learn it",
+    },
+    {
+      title: 'Make a Kanban App',
+      description: "Make a Kanban App using Angular and Firebase"
+    }
+  ]
 }
